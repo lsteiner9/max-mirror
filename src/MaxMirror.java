0bdfab8070569f23bc16ac994c3de5854142331a
lsteiner9 <lsteiner1@cnm.edu>
2021-03-22 16:23:44 -0600

    Initial commit

---
+++ src/MaxMirror.java
@@ -0,0 +1,30 @@
+public class MaxMirror {
+
+  public int maxMirror(int[] nums) {
+    int count = nums.length;
+    if (nums.length == 0) {
+    } else if (nums[0] != nums [nums.length - 1]) {
+      count = Math.max(recurseMirror(nums, 0, nums.length - 2), recurseMirror(nums, 1, nums.length - 1));
+    }
+    return count;
+  }
+
+  private int recurseMirror(int[] nums, int start, int stop) {
+    if (start >= stop) {
+      return 1;
+    } else if (!isMirror(nums, start, stop)) {
+      return Math.max(recurseMirror(nums, start + 1, stop), recurseMirror(nums, start, stop - 1));
+    } else {
+      return nums.length;
+    }
+  }
+
+  private boolean isMirror(int[]nums, int start, int stop) {
+    for (int i = 0, j = stop + 1 - start; j >= 0; i++, j--) {
+      if (nums[i] != nums[j]) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
